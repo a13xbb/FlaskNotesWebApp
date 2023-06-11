db1586a0c49b7f9f1ee9f40630eefda29dfbe326
Alex Blokh <77462141+a13xbb@users.noreply.github.com>
2023-06-11 17:09:17 +0300

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,16 +1,16 @@
-from . import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
+from . import db # type: ignore
+from flask_login import UserMixin # type: ignore
+from sqlalchemy.sql import func # type: ignore
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+class User(db.Model, UserMixin): # type: ignore
+    id = db.Column(db.Integer, primary_key=True) # type: ignore
+    email = db.Column(db.String(150), unique=True) # type: ignore
+    password = db.Column(db.String(150)) # type: ignore
+    first_name = db.Column(db.String(150)) # type: ignore
+    notes = db.relationship('Note') # type: ignore
     
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class Note(db.Model): # type: ignore
+    id = db.Column(db.Integer, primary_key=True) # type: ignore
+    data = db.Column(db.String(10000)) # type: ignore
+    date = db.Column(db.DateTime(timezone=True), default=func.now()) # type: ignore
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # type: ignore
